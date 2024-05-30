401da3e5c6fe64894664feea32dc5e7a50505bd2
murdex5 <fkadupitige@gmail.com>
2024-05-30 14:08:59 +0200

    added main.cpp file

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "core_functions.cpp"
+using namespace::std;
+
+int main(void) {
+    float added_value = add(2.4, 7.44);
+    cout<<"Value "<< added_value<<endl;
+    return 0;
+}
